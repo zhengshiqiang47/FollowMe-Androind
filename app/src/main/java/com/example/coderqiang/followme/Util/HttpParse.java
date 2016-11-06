2e2cf17c4107838c4ec79842e51cd8bfc42c8b76
CoderQiang <976928202@qq.com>
2016-11-06 14:30:14 +0800

    完成广场界面搭建

---
+++ app/src/main/java/com/example/coderqiang/followme/Util/HttpParse.java
@@ -0,0 +1,12 @@
+package com.example.coderqiang.followme.Util;
+
+/**
+ * Created by CoderQiang on 2016/11/6.
+ */
+
+public class HttpParse {
+
+    public static void getScenicspot(String cityname) {
+        
+    }
+}
